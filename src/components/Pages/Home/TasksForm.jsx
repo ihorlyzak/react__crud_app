f0ce4119460fdd1042da062f4645b260a8cd64a7
ihor <mr.ihor.lyzak@gmail.com>
2022-01-13 11:24:00 +0200

    implemented delete task function

--- src/components/Pages/Home/TasksForm.jsx
+++ src/components/Pages/Home/TasksForm.jsx
@@ -1,97 +1,115 @@
 import { useState } from "react";
 import { useTasksList } from "hooks/useTasksList/useTasksList";
 import {
-    Button,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableRow,
-    Paper,
-    Box,
-    TextField,
+	Button,
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableHead,
+	TableRow,
+	Paper,
+	Box,
+	TextField,
 } from "@mui/material";
 
 export const TasksForm = () => {
-    // change input value
-    const [inputValue, setInputValue] = useState("");
+	// change input value
+	const [inputValue, setInputValue] = useState("");
 
-    const handleOnChange = (e) => {
-        setInputValue(e.target.value);
-    };
+	const handleOnChange = (e) => {
+		setInputValue(e.target.value);
+	};
 
-    // add new task & save in local storage
-    const [tasksList, setTasksList] = useTasksList();
+	// add new task & save in local storage
+	const [tasksList, setTasksList] = useTasksList();
 
-    const handleOnClick = () => {
-        if (inputValue.length) {
-            setTasksList((currentArray) => [...currentArray, inputValue]);            
-        }
-    };
+	const handleOnClick = () => {
+		if (inputValue.length) {
+			setTasksList((currentArray) => [...currentArray, inputValue]);
+			setInputValue("");
+		}
+	};
 
-    return (
-        // input field & button
-        <div>
-            <Box
-                component="form"
-                sx={{
-                    margin: 1,
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                }}
-            >
-                <div>
-                    <TextField
-                        sx={{ minWidth: "180px", margin: "0 15px 0 0" }}
-                        id="outlined-basic "
-                        label="Enter new task "
-                        variant="outlined"
-                        size="small"
-                        type="text"
-                        name="username"
-                        onChange={handleOnChange}
-                        value={inputValue}
-                    />
-                    <Button
-                        sx={{
-                            border: "1px solid #bce0fd",
-                            minWidth: "180px",
-                            margin: "3px 0",
-                        }}
-                        size="small"
-                        variant="text"
-                        color="success"
-                        onClick={handleOnClick}
-                    >
-                        Save task
-                    </Button>
-                </div>
-            </Box>
+	const handleOnDeleteClick = (taskValue) => {
+		const newTaskList = tasksList.filter((value) => value !== taskValue);
+		setTasksList(newTaskList);
+	};
 
-            {/* Table */}
-            <TableContainer component={Paper}>
-                <Table size="small" aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>My tasks:</TableCell>
-                            <TableCell align="right">Actions</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {tasksList.map((task) => (
-                            <TableRow key={task}>
-                                <TableCell> {task} </TableCell>
-                                <TableCell align="right">
-                                    <Button>Edit</Button>
-                                    <Button color="error">Delete</Button>
-                                </TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </div>
-    );
+	return (
+		// input field & button
+		<div>
+			<Box
+				component="form"
+				sx={{
+					margin: 1,
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
+				}}
+			>
+				<div>
+					<TextField
+						sx={{ minWidth: "180px", margin: "0 15px 0 0" }}
+						id="outlined-basic "
+						label="Enter new task "
+						variant="outlined"
+						size="small"
+						type="text"
+						name="username"
+						onChange={handleOnChange}
+						value={inputValue}
+					/>
+					<Button
+						sx={{
+							border: "1px solid #bce0fd",
+							minWidth: "180px",
+							margin: "3px 0",
+						}}
+						size="small"
+						variant="text"
+						color="success"
+						onClick={handleOnClick}
+					>
+						Save task
+					</Button>
+				</div>
+			</Box>
+
+			{/* Table */}
+			<TableContainer component={Paper}>
+				<Table size="small" aria-label="simple table">
+					{tasksList.length ? (
+						<TableHead>
+							<TableRow>
+								<TableCell>My tasks:</TableCell>
+								<TableCell align="right">Actions</TableCell>
+							</TableRow>
+						</TableHead>
+					) : (
+						<p>Task list is Empty</p>
+					)}
+
+					<TableBody>
+						{tasksList.map((task) => (
+							<TableRow key={task}>
+								<TableCell> {task} </TableCell>
+								<TableCell align="right">
+									<Button>Edit</Button>
+									<Button
+										color="error"
+										onClick={() =>
+											handleOnDeleteClick(task)
+										}
+									>
+										Delete
+									</Button>
+								</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</TableContainer>
+		</div>
+	);
 };
